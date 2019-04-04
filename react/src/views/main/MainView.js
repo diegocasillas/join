4a4f206af34c918e8e9be781dbfef1168a52c0b6
Oscar Blide <oscarblide@gmail.com>
2019-04-04 12:33:39 +0200

    Add simple footer

--- react/src/views/main/MainView.js
+++ react/src/views/main/MainView.js
@@ -4,18 +4,22 @@ import HomeView from '../home/HomeView'
 import LibraryView from '../library/LibraryView'
 import EventView from '../event/EventView'
 import Header from './header/Header'
+import Footer from './footer/Footer'
+import './mainView.css'
 
 class MainView extends Component {
   render() {
     return (
       <div className='MainView'>
         <Header />
-        <Router >
-          <Route exact path='/' render={() => <HomeView />} />
-          <Route exact path='/events' render={() => <LibraryView />} />
-          <Route path='/events/:id' render={(props) => <EventView {...props} />} />
-        </Router>
-        <header>Footer</header>
+        <div className="background">
+          <Router>
+            <Route exact path='/' render={() => <HomeView />} />
+            <Route exact path='/events' render={() => <LibraryView />} />
+            <Route path='/events/:id' render={(props) => <EventView {...props} />} />
+          </Router>
+        </div>
+        <Footer />
       </div>
     )
   }
