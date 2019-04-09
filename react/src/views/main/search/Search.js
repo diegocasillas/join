caf6a6121ed8ad308fdd20532f4c45ff2829867f
Oscar Blide <oscarblide@gmail.com>
2019-04-09 12:50:25 +0200

    Fix search bar style

--- react/src/views/main/search/Search.js
+++ react/src/views/main/search/Search.js
@@ -5,17 +5,16 @@ import searchIcon from './search_icon.png'
 class Search extends Component {
     render() {
         return (
-            <div className="Search">
-                <div className="input-group mb-3 col-10 col-md-4 mx-auto top-links">
+            <div className="Search col-10">
+                <div className="input-group mb-3 col-10 col-md-5 mx-auto top-links">
                     <div className="input-group-prepend">
                         <span className="rounded-0 input-group-text white" id="basic-addon1">
                             <img className="search-icon" src={searchIcon} width="20" height="20" />
                         </span>
                     </div>
-                    <div>
-                        <input type="text" className="rounded-0 form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
-                    </div>
+                    <input type="text" className="search-box rounded-0 form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
                 </div>
+
 
             </div>
         )
