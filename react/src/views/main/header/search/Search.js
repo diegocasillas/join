83e2206b9f065303b71c9ca7f5a3d03b39d26fd8
Oscar Blide <oscarblide@gmail.com>
2019-04-17 14:29:16 +0200

    Change of mainView (Design)

--- react/src/views/main/header/search/Search.js
+++ react/src/views/main/header/search/Search.js
@@ -5,11 +5,11 @@ import searchIcon from './search_icon.png'
 class Search extends Component {
     render() {
         return (
-            <div className="Search col-10">
+            <div className="Search col-10 border-0">
                 <div className="input-group mb-3 col-10 col-md-5 mx-auto top-links">
                     <div className="input-group-prepend">
-                        <span className="rounded-0 input-group-text white" id="basic-addon1">
-                            <img className="search-icon" src={searchIcon} width="20" height="20" />
+                        <span className="searchBox rounded-0 input-group-text" id="basic-addon1">
+                            <img className="search-icon" src={searchIcon} width="23" height="23" />
                         </span>
                     </div>
                     <input type="text" className="search-box rounded-0 form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" />
