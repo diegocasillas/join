5a171d072ec5c83210b118c1683cb1a2efe0f1f4
oblide <oscarblide@gmail.com>
2019-04-23 14:43:43 +0300

    Add home welcome logo

--- react/src/views/home/HomeView.js
+++ react/src/views/home/HomeView.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import welcomeMessage from './welcome-message.png'
 
 class HomeView extends Component {
   render () {
     return (
       <div className='HomeView'>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <h1>HOMEVIEW</h1>
-        <Link to='/events'>Events</Link>
+        <div className='text-center mt-5'>
+          <img src={welcomeMessage} />
+        </div>
+        <div className='text-center'>
+          <Link to='/events'><button className='btn btn-primary'>Events</button></Link>
+
+        </div>
       </div>
     )
   }
