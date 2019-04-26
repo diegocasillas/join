ae98043ceef1f2f3ccc477d742e64356a467a59e
Jyri Nygard <jyrinyge@gmail.com>
2019-04-26 13:25:27 +0300

    Login style

--- react/src/views/main/header/Header.js
+++ react/src/views/main/header/Header.js
@@ -15,14 +15,22 @@ class Header extends Component {
             <div className='row'>
 
               <div className='col-2'>
-                <Link to='/'><img className='logo' src={logo} alt='Responsive image' width='120' height='55' /></Link>
+                <Link to='/'><img className='logo' src={logo} alt='Responsive image' width='120' height='55' />
+                </Link>
               </div>
 
               <Search />
 
               <div className='col-2 top-links'>
                 <li className='loginButton nav-item  text-right'>
-                  <Link to={this.props.loggedIn ? '/logout' : '/login'}>{this.props.loggedIn ? 'Logout' : 'Login'} <img className='profile' src={profile} alt='Responsive image' width='55' height='55' /></Link>
+                  <Link
+                    style={{ color: 'white', fontSize: '20px' }}
+                    className='mark'
+                    to={this.props.loggedIn ? '/logout' : '/login'}
+                  >
+                    {this.props.loggedIn ? 'Logout' : 'Login'}
+                    <img className='profile' src={profile} alt='Responsive image' width='55' height='55' />
+                  </Link>
                 </li>
               </div>
 
