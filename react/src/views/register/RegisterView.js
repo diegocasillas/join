8b9c94ccffe1e2d597ed7aa2b13457e4c1602be0
Oscar Blide <oscarblide@gmail.com>
2019-04-29 11:11:29 +0200

    Fixed style to the register

--- react/src/views/register/RegisterView.js
+++ react/src/views/register/RegisterView.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import Auth from '../../Auth'
+import './registerViewStyle.css';
 
 class RegisterView extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.auth = new Auth()
     this.state = { name: '', email: '', password: '', toHome: false }
   }
 
-  handleChange (field, value) {
+  handleChange(field, value) {
     this.setState({ [field]: value })
   }
 
-  register () {
+  register() {
     this.auth.register(this.state.name, this.state.email, this.state.password)
       .then(() => {
         this.props.toggleLogin()
@@ -21,7 +22,7 @@ class RegisterView extends Component {
       })
   }
 
-  render () {
+  render() {
     if (this.state.toHome) {
       this.setState({ toHome: false })
       return <Redirect to={`/`} />
@@ -29,20 +30,22 @@ class RegisterView extends Component {
 
     return (
       <div className='RegisterView'>
-        <div className='form-group'>
-          <input type='text' className='form-control' id='name' placeholder='Username' value={this.state.name} onChange={(event) => this.handleChange('name', event.target.value)} />
-        </div>
+        <div className='col-4 mt-5 mx-auto'>
+          <div className='form-group'>
+            <input type='text' className='form-control border-0 rounded-0 mb-3 mt-4 col-12 mx-auto' id='name' placeholder='Username' value={this.state.name} onChange={(event) => this.handleChange('name', event.target.value)} />
+          </div>
 
-        <div className='form-group'>
-          <input type='text' className='form-control' id='email' placeholder='Email' value={this.state.email} onChange={(event) => this.handleChange('email', event.target.value)} />
-        </div>
+          <div className='form-group'>
+            <input type='text' className='form-control border-0 rounded-0 mb-3 mt-4 col-12 mx-auto' id='email' placeholder='Email' value={this.state.email} onChange={(event) => this.handleChange('email', event.target.value)} />
+          </div>
 
-        <div className='form-group'>
-          <input type='password' className='form-control' id='password' placeholder='Password' value={this.state.password} onChange={(event) => this.handleChange('password', event.target.value)} />
-        </div>
+          <div className='form-group'>
+            <input type='password' className='form-control border-0 rounded-0 mb-3 mt-4 col-12 mx-auto' id='password' placeholder='Password' value={this.state.password} onChange={(event) => this.handleChange('password', event.target.value)} />
+          </div>
 
-        <div className='form-group'>
-          <button className='btn btn-warning text-light mr-2' onClick={() => this.register()}>Register</button>
+          <div className='form-group text-center'>
+            <button className='registerButton btn btn-warning text-light mr-3 mt-4' onClick={() => this.register()}>Register</button>
+          </div>
         </div>
       </div>
     )
